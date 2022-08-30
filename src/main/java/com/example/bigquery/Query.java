7c4f42d16844254076aaef72ef45bd80fe6fa463
A838102 <adrian.blazquez@atos.net>
2022-08-30 13:26:56 +0000

    get the request

--- src/main/java/com/example/bigquery/Query.java
+++ src/main/java/com/example/bigquery/Query.java
@@ -29,11 +29,13 @@ import com.amazonaws.services.costexplorer.AWSCostExplorerClientBuilder;
 import com.amazonaws.services.costexplorer.model.*;
 
 import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.rest.Response;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.core.util.Context;
 import com.azure.resourcemanager.costmanagement.CostManagementManager;
+import com.azure.resourcemanager.costmanagement.implementation.QueryResultImpl;
 import com.azure.resourcemanager.costmanagement.models.ExportType;
 import com.azure.resourcemanager.costmanagement.models.FunctionType;
 import com.azure.resourcemanager.costmanagement.models.GranularityType;
@@ -42,6 +44,7 @@ import com.azure.resourcemanager.costmanagement.models.QueryColumnType;
 import com.azure.resourcemanager.costmanagement.models.QueryDataset;
 import com.azure.resourcemanager.costmanagement.models.QueryDefinition;
 import com.azure.resourcemanager.costmanagement.models.QueryGrouping;
+import com.azure.resourcemanager.costmanagement.models.QueryResult;
 import com.azure.resourcemanager.costmanagement.models.TimeframeType;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -100,7 +103,7 @@ public class Query {
     BasicConfigurator.configure();
 
     try{
-        subscriptionQueryLegacy(manager);
+         subscriptionQueryTraza(manager) ;
         //customerQueryGroupingModern(manager);
     }
      catch (final Exception e) {
@@ -110,46 +113,6 @@ public class Query {
   }
 
   //https://github.com/Azure/azure-sdk-for-java/blob/main/sdk/costmanagement/azure-resourcemanager-costmanagement/src/samples/java/com/azure/resourcemanager/costmanagement/QueryUsageSamples.java
-      /**
-     * Sample code: CustomerQueryGrouping-Modern.
-     *
-     * @param costManagementManager Entry point to CostManagementManager.
-     */
-    public static void customerQueryGroupingModern(
-        com.azure.resourcemanager.costmanagement.CostManagementManager costManagementManager) {
-        costManagementManager
-            .queries()
-            .usageWithResponse(
-                "providers/Microsoft.Billing/billingAccounts/0b6b4c37-f1bf-4ce2-a367-85ec50c803ea/customers/fbb4764e-16a0-454a-8adc-54b94dac24f6",
-                new QueryDefinition()
-                    .withType(ExportType.USAGE)
-                    .withTimeframe(TimeframeType.THE_LAST_MONTH)
-                    .withDataset(
-                        new QueryDataset()
-                            .withGranularity(GranularityType.fromString("None"))
-                            .withAggregation(
-                                mapOf(
-                                    "totalCost",
-                                    new QueryAggregation().withName("PreTaxCost").withFunction(FunctionType.SUM)))
-                            .withGrouping(
-                                Arrays
-                                    .asList(
-                                        new QueryGrouping()
-                                            .withType(QueryColumnType.DIMENSION)
-                                            .withName("ResourceGroup")))),
-                Context.NONE);
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T> Map<String, T> mapOf(Object... inputs) {
-        Map<String, T> map = new HashMap<>();
-        for (int i = 0; i < inputs.length; i += 2) {
-            String key = (String) inputs[i];
-            T value = (T) inputs[i + 1];
-            map.put(key, value);
-        }
-        return map;
-    }
 
     /**
      * Este código funciona en la función de prueba de azure
@@ -157,10 +120,12 @@ public class Query {
      * Sample code: SubscriptionQuery-Legacy.
      *
      * @param costManagementManager Entry point to CostManagementManager.
+     * @return 
      */
-    public static void subscriptionQueryLegacy(
+    public static void subscriptionQueryTraza(
         com.azure.resourcemanager.costmanagement.CostManagementManager costManagementManager) {
-        costManagementManager
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!! Resultado query Azure "+ 
+            costManagementManager
             .queries()
             .usageWithResponse(
                 "subscriptions/0b6b4c37-f1bf-4ce2-a367-85ec50c803ea",
@@ -171,7 +136,8 @@ public class Query {
                         new QueryDataset()
                             .withGranularity(GranularityType.DAILY)
                            ),
-                Context.NONE);
+                Context.NONE).getRequest().getBody()
+        );
     }
 
     // Inspired by this comment https://gist.github.com/vatshat/f3fa2bbee59edcabf3d9cb4b04d88c72?permalink_comment_id=4184585
