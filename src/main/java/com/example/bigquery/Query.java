d0f3b5be40c29f609b2bed7cf527db04f2ba1807
a838102 <adrian.blazquez@atos.net>
2022-08-30 11:37:19 +0200

    aws and gcp are back

--- src/main/java/com/example/bigquery/Query.java
+++ src/main/java/com/example/bigquery/Query.java
@@ -16,8 +16,17 @@
 
 package com.example.bigquery;
 
-// [START bigquery_query]
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryException;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.TableResult;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.costexplorer.AWSCostExplorer;
+import com.amazonaws.services.costexplorer.AWSCostExplorerClientBuilder;
+import com.amazonaws.services.costexplorer.model.*;
 
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.management.AzureEnvironment;
@@ -44,7 +53,39 @@ public class Query {
 
   public static void main(String[] args) {
 
-    // Azure
+      //GCP
+
+      // TODO(developer): Replace these variables before running the sample.
+      String projectId = "atos-iberia-idm-cloud-demo-1";
+      String datasetName = "billing_data";
+      String tableName = "gcp_billing_export_v1_01FF7F_D1770F_4DEB3A";
+      String query =
+              "SELECT invoice.month,\n"
+                      + " SUM(cost)"
+                      + " + SUM(IFNULL((SELECT SUM(c.amount) FROM UNNEST(credits) c), 0))"
+                      + " AS total,"
+                      + " (SUM(CAST(cost * 1000000 AS int64))"
+                      + " + SUM(IFNULL((SELECT SUM(CAST(c.amount * 1000000 as int64)) FROM UNNEST(credits) c), 0))) / 1000000"
+                      + " AS total_exact"
+                      + " FROM `"
+                      + projectId
+                      + "."
+                      + datasetName
+                      + "."
+                      + tableName
+                      + "`"
+                      + " GROUP BY 1"
+                      + " ORDER BY 1 ASC";
+
+
+      // AWS
+      CEWithDimension();
+
+      //GCP
+      query(query);
+
+
+      // Azure
     AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
     TokenCredential credential = new DefaultAzureCredentialBuilder()
             .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
@@ -133,6 +174,52 @@ public class Query {
                 Context.NONE);
     }
 
+    // Inspired by this comment https://gist.github.com/vatshat/f3fa2bbee59edcabf3d9cb4b04d88c72?permalink_comment_id=4184585
+    private static void CEWithDimension() {
+        Expression expression = new Expression();
+        DimensionValues dimensions = new DimensionValues();
+        dimensions.withKey(Dimension.SERVICE);
+        dimensions.withValues("Amazon Route 53");
+
+        expression.withDimensions(dimensions);
+
+        final GetCostAndUsageRequest awsCERequest = new GetCostAndUsageRequest()
+                .withTimePeriod(new DateInterval().withStart("2022-06-01").withEnd("2022-06-30"))
+                .withGranularity(Granularity.DAILY)
+                .withMetrics("BlendedCost")
+                .withFilter(expression);
+
+        try {
+            AWSCostExplorer ce = AWSCostExplorerClientBuilder.standard()
+                    //.withCredentials(new CredentialsClient().getCredentials())
+                    .build();
+
+            System.out.println(ce.getCostAndUsage(awsCERequest));
+
+        } catch (final Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void query(String query) {
+        try {
+            // Initialize client that will be used to send requests. This client only needs to be created
+            // once, and can be reused for multiple requests.
+            BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+
+            QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).build();
+
+            TableResult results = bigquery.query(queryConfig);
+
+            results
+                    .iterateAll()
+                    .forEach(row -> row.forEach(val -> System.out.printf("%s,", val.toString())));
+
+            System.out.println("Query performed successfully.");
+        } catch (BigQueryException | InterruptedException e) {
+            System.out.println("Query not performed \n" + e.toString());
+        }
+    }
 
 }
 
